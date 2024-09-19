d35ef70c0592c8166c87484913666e63177b6d06
Nizar El Khalili <nizar.el.khalili@accenture.com>
2024-09-19 15:14:10 +0200

    gpt model architecture

---
+++ attention_mechanism/multi_head_attention.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+
+
+class MultiHeadAttention(nn.Module):
+    """
+    Multi-Head Attention mechanism.
+
+    Args:
+        d_in (int): Input dimension.
+        d_out (int): Output dimension.
+        context_length (int): Length of the context.
+        num_heads (int): Number of attention heads.
+        dropout (float): Dropout rate.
+    """
+
+    def __init__(self, d_in, d_out, context_length, num_heads, dropout=0.1):
+        super().__init__()
+        assert d_out % num_heads == 0, "d_out must be divisible by num_heads"
+        self.num_heads = num_heads
+        self.d_out = d_out
+        self.head_dim = d_out // num_heads
+        self.query = nn.Linear(d_in, d_out, bias=False)
+        self.key = nn.Linear(d_in, d_out, bias=False)
+        self.value = nn.Linear(d_in, d_out, bias=False)
+        self.output = nn.Linear(d_out, d_out, bias=False)
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer(
+            "mask", torch.triu(torch.ones(context_length, context_length), diagonal=1)
+        )
+
+    def forward(self, x):
+        b, num_tokens, _ = x.shape
+
+        keys = self.key(x)  # Shape: (b, num_tokens, d_out)
+        queries = self.query(x)
+        values = self.value(x)
+
+        # We implicitly split the matrix by adding a `num_heads` dimension
+        # Unroll last dim: (b, num_tokens, d_out) -> (b, num_tokens, num_heads, head_dim)
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+
+        # Transpose: (b, num_tokens, num_heads, head_dim) -> (b, num_heads, num_tokens, head_dim)
+        keys = keys.transpose(1, 2)
+        queries = queries.transpose(1, 2)
+        values = values.transpose(1, 2)
+
+        # Compute scaled dot-product attention (aka self-attention) with a causal mask
+        attn_scores = queries @ keys.transpose(2, 3)  # Dot product for each head
+
+        # Original mask truncated to the number of tokens and converted to boolean
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+
+        # Use the mask to fill attention scores
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        # Shape: (b, num_tokens, num_heads, head_dim)
+        output = (attn_weights @ values).transpose(1, 2)
+
+        # Combine heads, where self.d_out = self.num_heads * self.head_dim
+        output = output.contiguous().view(b, num_tokens, self.d_out)
+        output = self.output(output)  # optional projection
+
+        return output
