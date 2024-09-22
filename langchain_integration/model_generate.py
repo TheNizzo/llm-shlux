eb7d5d58a4e7c67e64b12629a36d6cf770b858e6
Niss <khalili-nizar@hotmail.com>
2024-09-22 22:02:45 +0200

    langchain custom model integration

---
+++ langchain_integration/model_generate.py
@@ -0,0 +1,46 @@
+import torch
+
+def text_to_tokens(text, tokenizer):
+    encoding = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+    encoding_tensor = torch.tensor(encoding).unsqueeze(0)
+    return encoding_tensor
+
+def tokens_to_text(tokens, tokenizer):
+    text = tokenizer.decode(tokens.squeeze(0).tolist())
+    return text
+def generate(model, idx, max_new_tokens, context_size, temperature=0.0, top_k=None, eos_id=None):
+    # For-loop is the same as before: Get logits, and only focus on last time step
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+
+        # New: Filter logits with top_k sampling
+        if top_k is not None:
+            # Keep only top_k values
+            top_logits, _ = torch.topk(logits, top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(logits < min_val, torch.tensor(float("-inf")).to(logits.device), logits)
+
+        # New: Apply temperature scaling
+        if temperature > 0.0:
+            logits = logits / temperature
+
+            # Apply softmax to get probabilities
+            probs = torch.softmax(logits, dim=-1)  # (batch_size, context_len)
+
+            # Sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)  # (batch_size, 1)
+
+        # Otherwise same as before: get idx of the vocab entry with the highest logits value
+        else:
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)  # (batch_size, 1)
+
+        if idx_next == eos_id:  # Stop generating early if end-of-sequence token is encountered and eos_id is specified
+            break
+
+        # Same as before: append sampled index to the running sequence
+        idx = torch.cat((idx, idx_next), dim=1)  # (batch_size, num_tokens+1)
+
+    return idx
